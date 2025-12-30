0b53ba1489755454006245adc507d04d4334b145
joyjoy42 <akodekoumerveillejoy@gmail.com>
2025-12-30 13:27:11 +0000

    Remove Symfony project

--- BrasilBurger_Symfony/ADD_ARCHIVE_COLUMN.sql
+++
@@ -1,6 +0,0 @@
--- Run this SQL command on your PostgreSQL database to add the archive column
--- Copy and paste this into your database management tool
-
-ALTER TABLE burgers ADD COLUMN archive BOOLEAN NOT NULL DEFAULT false;
-
-
--- BrasilBurger_Symfony/ADD_UPDATED_AT_COLUMN.sql
+++
@@ -1,6 +0,0 @@
--- Add updated_at column to commandes table
--- Run this SQL in Neon's SQL Editor
-
-ALTER TABLE commandes ADD COLUMN IF NOT EXISTS date_modification TIMESTAMP NULL;
-
-
