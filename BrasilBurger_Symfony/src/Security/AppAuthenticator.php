a6cbedcede337bde20316eb27d07fb7323b52270
joyjoy42 <akodekoumerveillejoy@gmail.com>
2025-12-31 17:09:22 +0000

    Configuration sécurité et authentification

---
+++ BrasilBurger_Symfony/src/Security/AppAuthenticator.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\Gestionnaire;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\PasswordUpgradeBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class AppAuthenticator extends AbstractAuthenticator
+{
+    use TargetPathTrait;
+
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UrlGeneratorInterface $urlGenerator
+    ) {
+    }
+
+    public function supports(Request $request): ?bool
+    {
+        return 'app_login' === $request->attributes->get('_route')
+            && $request->isMethod('POST');
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $email = $request->request->get('email', '');
+        $request->getSession()->set('_security.last_username', $email);
+
+        return new Passport(
+            new UserBadge($email, function ($userIdentifier) {
+                $user = $this->entityManager->getRepository(Gestionnaire::class)->findOneBy(['email' => $userIdentifier]);
+
+                if (!$user) {
+                    throw new AuthenticationException('Email ou mot de passe incorrect.');
+                }
+
+                return $user;
+            }),
+            new PasswordCredentials($request->request->get('password', '')),
+            [
+                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
+                new RememberMeBadge(),
+            ]
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new JsonResponse(['target' => $targetPath]);
+        }
+
+        return new JsonResponse(['target' => $this->urlGenerator->generate('app_dashboard')]);
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        $data = [
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+}
