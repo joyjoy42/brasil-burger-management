bf7d113b0f9387835f8c271d08be6b49e04ffe6b
joyjoy42 <akodekoumerveillejoy@gmail.com>
2026-01-08 01:25:26 +0000

    feat(java): synchronize database and cloudinary configuration

--- java-app/src/com/brasilburger/repository/DatabaseUtils.java
+++ java-app/src/com/brasilburger/repository/DatabaseUtils.java
@@ -5,9 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtils {
-    private static final String URL = "jdbc:postgresql://localhost:5432/brasil_burger";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "password";
+    private static final String URL = "jdbc:postgresql://ep-fancy-mode-a4bg18kq-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require";
+    private static final String USER = "neondb_owner";
+    private static final String PASSWORD = "npg_fgvbm4Vze5oD";
+    public static final String CLOUDINARY_URL = "cloudinary://166294258315442:9bpSi55tkiP5IZnwNpHrMuw-Qsc@dbkji1d1j";
 
     static {
         try {
